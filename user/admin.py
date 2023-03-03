c1020d7ec53e23faf5ec4e5a0a7cb36f5b7555ed
de-ola <ademiluaadeola969@gmail.com>
2023-03-03 23:04:18 +0100

    first commit

---
+++ user/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(AdminHOD)
+admin.site.register(SessionYearModel)
+admin.site.register(Staffs)
+admin.site.register(StudentResult)
+admin.site.register(Students)
+admin.site.register(Subjects)
+admin.site.register(Courses)
+admin.site.register(User)
